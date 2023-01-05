acf601b1926fe2abb61774c11fbc2cbaf1796e2b
Osama Bin Hashim <zeeshan2k01@gmail.com>
2023-01-06 00:07:29 +0600

    initial commit

---
+++ app/src/main/java/com/zeeshan_s/travelmate/Activities/MainActivity.java
@@ -0,0 +1,40 @@
+package com.zeeshan_s.travelmate.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.zeeshan_s.travelmate.R;
+import com.zeeshan_s.travelmate.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    ArrayList<SlideModel> imageList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        ---------------------------- Image Slider ------------------------
+//        Image Slider data entry example___________
+//        imageList.add(SlideModel("String Url" or R.drawable, ScaleTypes.FIT) // for one image
+//        imageList.add(SlideModel("String Url" or R.drawable, "Title", ScaleTypes.FIT) // you can with title
+//        imageSlider.setImageList(imageList, ScaleTypes.FIT) // for all images
+
+        imageList = new ArrayList<>();
+        imageList.add(new SlideModel(R.drawable.flower_garden_img,"Garden", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.tree_and_light_img,"Night", ScaleTypes.CENTER_CROP));
+        imageList.add(new SlideModel(R.drawable.mars_human_colony_img,"Mars", ScaleTypes.CENTER_CROP));
+
+        binding.imageSlider.setImageList(imageList);
+    }
+}
